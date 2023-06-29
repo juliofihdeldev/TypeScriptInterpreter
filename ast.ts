fb1532ab85677b2594ea5983148d6ea8f133e83d
juliofils <julio.fils1@gmail.com>
2023-06-29 18:03:02 -0400

    add test for it

---
+++ ast.ts
@@ -0,0 +1,35 @@
+// deno-lint-ignore-file
+
+export type NodeType = 'Program'
+    | 'NumericLiteral'
+    | 'BinaryExpr'
+    | 'Identifier';
+
+// rules statement is not expression
+export interface Statement {
+    kind: NodeType;
+}
+
+export interface Program extends Statement {
+    kind: 'Program';
+    body: Statement[];
+}
+
+export interface Expr extends Statement { }
+
+export interface BinaryExpr extends Expr {
+    kind: 'BinaryExpr';
+    operator: string;
+    left: Expr;
+    right: Expr;
+}
+
+export interface Identifier extends Expr {
+    kind: 'Identifier';
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: 'NumericLiteral';
+    value: number;
+}
